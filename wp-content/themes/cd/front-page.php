cce52383797eae94eb4cac8421e50bc7d62bafda
WEBzaytsev <studio@webzaytsev.ru>
2022-08-16 23:40:38 +0200

    add five section

--- wp-content/themes/cd/front-page.php
+++ wp-content/themes/cd/front-page.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-    <main class="relative z-10 container grid gap-y-4">
+    <main class="relative z-10 container grid gap-y-4 mb-4">
 
         <!--First section-->
         <?php if (have_rows('main-banner')) : ?>
@@ -196,6 +196,38 @@ get_header(); ?>
             </div>
         <?php endif; ?>
         <!--End four section-->
+
+        <!--Five section-->
+        <?php if (have_rows('about')) : ?>
+            <?php while (have_rows('about')) : the_row(); ?>
+                <?php $img = get_sub_field('img'); ?>
+                <?php $color = get_sub_field('color'); ?>
+                <div class="pt-12.5 pl-9 h-[600px] bg-white rounded-30 bg-cover text-white"
+                     style="background:linear-gradient(100.94deg, rgba(<?php echo formatColor($color); ?> 1) 32.74%, rgba(<?php echo formatColor($color); ?> 0) 60.15%),
+                             url('<?php echo esc_url($img['url']); ?>') no-repeat center;">
+                    <p class="font-semibold tracking-tight text-5xl leading-[58px] mb-12">
+                        <?php the_sub_field('caption'); ?>
+                    </p>
+                    <p class="font-medium text-xl">
+                        <?php the_sub_field('text'); ?>
+                    </p>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        <!--End five section-->
     </main>
 
 <?php get_footer();
+
+function formatColor($rgba_color_array): string
+{
+    $i = 1;
+    return trim(array_reduce($rgba_color_array, function($carry, $item) use ($rgba_color_array, &$i) {
+        if ($i >= count($rgba_color_array)) {
+            return $carry;
+        }
+
+        $i++;
+        return $carry .= $item . ', ';
+    }));
+}
