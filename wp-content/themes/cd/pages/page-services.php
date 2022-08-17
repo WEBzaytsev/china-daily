8da0792b3fe174a6356d0337afc42c2f45a070d3
WEBzaytsev <studio@webzaytsev.ru>
2022-08-17 18:59:31 +0200

    create services page

---
+++ wp-content/themes/cd/pages/page-services.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Услуги
+ */
+
+get_header();
+?>
+
+    <main class="relative z-10 container grid gap-y-4 mb-4">
+        <?php get_template_part('/template-parts/main-banner'); ?>
+
+        <?php get_template_part('/template-parts/services-list', null, array('page' => 'services')); ?>
+
+        <?php get_template_part('/template-parts/feedback-btn') ?>
+    </main>
+
+<?php
+get_footer();
