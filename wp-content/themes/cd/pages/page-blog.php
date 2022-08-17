ee3af2520a8dcd556046e8f2b337fa18d5aa204e
WEBzaytsev <studio@webzaytsev.ru>
2022-08-17 18:59:43 +0200

    create blog page

---
+++ wp-content/themes/cd/pages/page-blog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Блог
+ */
+
+get_header();
+?>
+
+    <main class="relative z-10 container mb-4">
+        <?php get_template_part('/template-parts/main-banner'); ?>
+    </main>
+
+<?php
+get_footer();
