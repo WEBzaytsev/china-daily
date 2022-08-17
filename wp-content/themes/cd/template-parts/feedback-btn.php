93469f600eb65a3835ea8069fa54361b1b038616
WEBzaytsev <studio@webzaytsev.ru>
2022-08-17 18:57:38 +0200

    separate feedback button

---
+++ wp-content/themes/cd/template-parts/feedback-btn.php
@@ -0,0 +1,4 @@
+<a href="#"
+   class="w-full block text-white py-9.5 bg-green-600 hover:bg-green-700 shadow-btn transition-all duration-300 font-medium text-xl tracking-small text-center rounded-30">
+    <?php esc_html_e('Оставить  заявку', 'cd'); ?>
+</a>
