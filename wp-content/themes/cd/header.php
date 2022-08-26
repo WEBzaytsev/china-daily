7a9d1a7a5c487733784fed77f769bd4a6039408e
WEBZaytsev <75935616+WEBzaytsev@users.noreply.github.com>
2022-08-26 13:17:44 +0300

    favicon

--- wp-content/themes/cd/header.php
+++ wp-content/themes/cd/header.php
@@ -21,6 +21,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/public_html/wp-content/themes/cd/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public_html/wp-content/themes/cd/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public_html/wp-content/themes/cd/favicon/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/public_html/wp-content/themes/cd/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/public_html/wp-content/themes/cd/favicon/mstile-144x144.png">
+<meta name="theme-color" content="#D80126">
+
+
     <?php wp_head(); ?>
 </head>
 
